cf4a30f4c07ebc43476d4b5a305d73a08e1a0bb3
Markus Linderbäck <markus@linderback.com>
2013-06-27 08:06:01 +0200

    Lagt till så det går uppdatera kalendern från Console-applikation

---
+++ MarkusConsoleApplication/UppdateraKalendern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    static class UppdateraKalendern
+    {
+        public static void Kör()
+        {
+            new MarkusModel.Kalender(DateTime.Today.Year).CreateCalendar(6);
+        }
+    }
+}
