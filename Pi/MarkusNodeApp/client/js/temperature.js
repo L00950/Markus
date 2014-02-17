a8599d48bb5968b7288d39d982835e70ff941007
Markus Linderbäck <markus@linderback.com>
2014-02-17 08:35:20 +0100

    Testat att lägga till webcam

--- Pi/MarkusNodeApp/client/js/temperature.js
+++ Pi/MarkusNodeApp/client/js/temperature.js
@@ -5,6 +5,7 @@ temperature.prototype.run = function() {
   // Prepare DOM-elements
     var container_temp = $('<div class="container container-400" id="sensor_container">');
   container_temp.append('<div class="box box-w-400 box-h-500 bg-dark" id="larmhistory_container">');
+  //container_temp.append('<div class="box box-w-400 box-h-500 bg-dark" id="cam_container">');
   //container_temp.append('<div class="box box-w-400 box-h-200 bg-dark" id="eliq_chart_dataday">');
   //container_temp.append('<div class="box box-w-200 box-h-100 bg-dark" id="spot_1">');
   //container_temp.append('<div class="box box-w-200 box-h-100 bg-dark" id="eliq_datanow">');
