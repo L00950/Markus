e5a19b9819bf85e641ad6c08707c9b73f95d3f8b
Markus Linderbäck <markus@linderback.com>
2014-03-06 17:54:55 +0100

    Ny client-kod till raspen

---
+++ Pi/MarkusNodeApp/Client.js
@@ -0,0 +1,16 @@
+﻿var net = require('net');
+
+var client = new net.Socket();
+client.connect(8089, 'linderback.com', function() {
+    console.log('CONNECTED...');
+    client.write('enablevpn');
+});
+
+client.on('data', function(data) {
+    console.log('DATA: ' + data);
+    client.destroy();
+});
+
+client.on('close', function() {
+    console.log('Connection closed');
+});
