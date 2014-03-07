80ed85f18d285ed31c5042c571aff13c170da3f1
Markus Linderbäck <markus@linderback.com>
2014-03-07 16:29:04 +0100

    Start / Stop av VPN Client

--- Pi/MarkusNodeApp/Client.js
+++ Pi/MarkusNodeApp/Client.js
@@ -1,14 +1,20 @@
 ﻿var net = require('net');
+var exec = require('child_process').exec;
 
 var client = new net.Socket();
 client.connect(8089, 'linderback.com', function() {
-    console.log('CONNECTED...');
     client.write('enablevpn');
 });
 
 client.on('data', function(data) {
-    console.log('DATA: ' + data);
+    console.log('enablevpn: ' + data);
     client.destroy();
+    if (data === '1')
+        exec('pon lidingo', function(error, stdout, stderr) {
+        });
+    else if (data === '0')
+        exec('poff lidingo', function(error, stdout, stderr) {
+        });
 });
 
 client.on('close', function() {
