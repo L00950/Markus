b60d698fe9c998cca9ef13f07b2dfa6b543426b8
Markus Linderbäck <markus@linderback.com>
2014-03-17 13:02:27 +0100

    MarkusArray fixad så att en array med en post blir rätt

--- Pi/MarkusNodeApp/MarkusArray.js
+++ Pi/MarkusNodeApp/MarkusArray.js
@@ -1,7 +1,7 @@
 ﻿module.exports = {
     insertFirst: function(array, item) {
         var antal = array.length;
-        for (var i = antal - 1; i > 0; i--) {
+        for (var i = antal; i > 0; i--) {
             array[i] = array[i - 1];
         }
         array[0] = item;
--- Pi/MarkusNodeApp/Test.js
+++ Pi/MarkusNodeApp/Test.js
@@ -1,3 +1,7 @@
-﻿var mail = require('./markusmail');
+﻿//var mail = require('./markusmail');
 
-mail.sendmail('markus@linderback.com', 'markus@linderback.com', 'Testmail', 'Innehåll...');
+//mail.sendmail('markus@linderback.com', 'markus@linderback.com', 'Testmail', 'Innehåll...');
+var markusarray = require('./markusarray');
+var l = ['B', 'C', 'D'];
+var l2 = markusarray.insertFirst(l, 'A');
+console.log(l2.length);
