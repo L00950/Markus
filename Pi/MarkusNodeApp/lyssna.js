32151e3d04274a01cb19adcb713af5acfaaa2f58
Markus Linderbäck <markus@linderback.com>
2014-04-30 16:12:17 +0200

    Lagt till funktion för att lyssna efter telldus-event

---
+++ Pi/MarkusNodeApp/lyssna.js
@@ -0,0 +1,16 @@
+﻿var telldus = require('telldus');
+
+
+telldus.addDeviceEventListener(function(device, status) {
+    console.log('DeviceEvent:' + device + ' Status:' + status);
+});
+
+telldus.addRawDeviceEventListener(function(id, data) {
+    console.log('RawDeviceEvent:' + data);
+});
+
+telldus.addSensorEventListener(function(id, protocol, model, type, value, ts) {
+    console.log('SensorEvent: ' + protocol + " " + model + " " + value + " " + id + " " + ts);
+});
+
+console.log('Lyssnar...');
