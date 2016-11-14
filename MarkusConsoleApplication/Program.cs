b9c1aa822614a36843a965268c7f5545b7c49114
Markus Linderbäck <markus@linderback.com>
2016-11-14 20:22:32 +0100

    Ny version av iTestSharp mm

--- MarkusConsoleApplication/Program.cs
+++ MarkusConsoleApplication/Program.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Linq;
+using System.Text;
 using System.Web.Script.Serialization;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 
 namespace MarkusConsoleApplication
 {
@@ -8,7 +14,7 @@ namespace MarkusConsoleApplication
     {
         static void Main(string[] args)
         {
-            if(args.Any(_ => _.Equals("BBS")))
+            if (args.Any(_ => _.Equals("BBS")))
                 LäsInMedlemmar.Kör();
             else if (args.Any(_ => _.Equals("UppdateraKalender")))
             {
@@ -16,7 +22,7 @@ namespace MarkusConsoleApplication
                 kalender.SkapaKalendrarFörMonteRojo();
                 kalender.SkapaKalendrarFörRyda();
             }
-            else if (args.Any(_ => _.Equals("test")))
+            else if (args.Any(_ => _.Equals("maila")))
             {
                 try
                 {
@@ -31,13 +37,82 @@ namespace MarkusConsoleApplication
             }
             else if (args.Any(_ => _.Equals("json")))
             {
-                var o = new TestKlass{Datum = DateTime.Now, Double = 123.456, Sträng = "Markus Linderbäck"};
+                var o = new TestKlass { Datum = DateTime.Now, Double = 123.456, Sträng = "Markus Linderbäck" };
                 var ser = new JavaScriptSerializer();
                 Console.WriteLine(ser.Serialize(o));
             }
-            else if(args.Any(_ => _.Equals("PDF")))
+            else if (args.Any(_ => _.Equals("PDF")))
             {
                 TestaLäsaPdf();
+            }
+            else if (args.Any(_ => _.Equals("test")))
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(Dns.GetHostAddresses("markuspi")[0], 8089); //Connect to the server on our local host IP address, listening to port 3000
+                
+                NetworkStream clientStream = client.GetStream();
+                var buffer = Encoding.ASCII.GetBytes("cache");
+                clientStream.Write(buffer, 0, buffer.Count());
+
+                System.Threading.Thread.Sleep(1000); //Sleep before we get the data for 1 second
+                while (clientStream.DataAvailable)
+                {
+                    byte[] inMessage = new byte[2048];
+                    int bytesRead = 0;
+                    try
+                    {
+                        bytesRead = clientStream.Read(inMessage, 0, 2048);
+                    }
+                    catch { /*Catch exceptions and handle them here*/ }
+
+                    ASCIIEncoding encoder = new ASCIIEncoding();
+                    Console.WriteLine(encoder.GetString(inMessage, 0, bytesRead));
+                }
+
+                client.Close();
+                System.Threading.Thread.Sleep(10000); //Sleep for 10 seconds
+
+
+                ConsoleApplication.AsynchronousClient.StartClient();
+
+
+                byte[] bytes = new byte[2048];
+                // Establish the remote endpoint for the socket.
+                // This example uses port 11000 on the local computer.
+                //IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostAddresses("markuspi"));
+                IPAddress ipAddress = Dns.GetHostAddresses("markuspi")[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8089);
+
+                // Create a TCP/IP  socket.
+                Socket sender = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.
+                    byte[] msg = Encoding.ASCII.GetBytes("cache");
+
+                    // Send the data through the socket.
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket.
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch(Exception exception)
+                {
+                }
             }
         }
 
