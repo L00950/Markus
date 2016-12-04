b159d81b70347cfbb1f580a20db8f07a9dce6754
Markus Linderbäck <markus@linderback.com>
2016-12-04 20:28:20 +0100

    Rättningar 4G trafikprogram

--- MarkusConsoleApplication/Spanien.cs
+++ MarkusConsoleApplication/Spanien.cs
@@ -18,11 +18,11 @@ namespace MarkusConsoleApplication
                 {
                     var stream = response.GetResponseStream();
                     var reader = new StreamReader(stream);
-                    var xml = reader.ReadToEnd();
+                    var xml = reader.ReadToEnd().Replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "");
                     var doc = new XmlDocument();
                     doc.LoadXml(xml);
-                    var upload = Convert.ToUInt32(doc.SelectSingleNode("TotalUpload").InnerText);
-                    var download = Convert.ToUInt32(doc.SelectSingleNode("TotalDownload").InnerText);
+                    var upload = Convert.ToUInt64(doc.SelectSingleNode("//response/TotalUpload").InnerText);
+                    var download = Convert.ToUInt64(doc.SelectSingleNode("//response/TotalDownload").InnerText);
 
                     var trafikdata = MarkusModel.FilHanterare.Läs<Trafik>(new Trafik().FilNamn()).ToList();
                     trafikdata.Add(new Trafik {Tidpunkt = DateTime.Now, UpLoad = upload, DownLoad = download});
@@ -40,10 +40,10 @@ namespace MarkusConsoleApplication
     public class Trafik : MarkusModel.IFilKlass
     {
         public DateTime Tidpunkt { get; set; }
-        public UInt32 UpLoad { get; set; }
-        public UInt32 DownLoad { get; set; }
+        public UInt64 UpLoad { get; set; }
+        public UInt64 DownLoad { get; set; }
 
-        public UInt32 Total()
+        public UInt64 Total()
         {
             return UpLoad + DownLoad;
         }
